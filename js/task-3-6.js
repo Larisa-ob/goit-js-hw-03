ae3687f30803b07e6e28130396352746eeb1f051
Larisa-ob <66275364+Larisa-ob@users.noreply.github.com>
2020-10-03 22:30:30 +0300

    task-3-5

--- js/task-3-6.js
+++ js/task-3-6.js
@@ -1,8 +1,14 @@
 function getAllPropValues(array, prop) {
-  const arrays = Object.values(array);
-  console.table(arrays);
+  let getALL = [];
+  let keys = prop;
+  if (array.length == 0) return getALL;
+  for (let i = 0; i < array.length; i += 1) {
+    if (typeof array[0][keys] === "undefined" || array[0][keys] === null)
+      return getALL;
+    getALL.push(array[i][keys]);
+  }
+  return getALL;
 }
-
 // Объекты и ожидаемый результат
 const products = [
   { name: "Радар", price: 1300, quantity: 4 },
@@ -14,11 +20,11 @@ const products = [
   { name: "Захват", price: 1200, quantity: 2 },
 ];
 
-//console.log(getAllPropValues(products, 'name'));
+console.log(getAllPropValues(products, "name"));
 // ['Радар', 'Радар', 'Радар', 'Сканер', 'Сканер', 'Дроид', 'Захват']
 
-//console.log(getAllPropValues(products, 'quantity'));
+console.log(getAllPropValues(products, "quantity"));
 // [4, 2, 1, 1, 3, 7, 2]
 
-//console.log(getAllPropValues(products, 'category'));
-//  []
+console.log(getAllPropValues(products, "category"));
+// not element
